b3d9d4a2d7a6ded30779dbf3de61adff8a78d84e
alingse <alingse@foxmail.com>
2022-05-08 17:29:14 +0800

    add fixtures && add tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,4 +1,5 @@
 import os
+import glob
 
 from antlr4 import FileStream
 from antlr4 import CommonTokenStream
@@ -12,18 +13,28 @@ from thrift_parser.ThriftParser import ThriftParser
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-
 def parse_one_thrift(filepath):
     file_abspath = os.path.abspath(os.path.join(TEST_DIR, filepath))
-    input_stream = FileStream(file_abspath)
+    input_stream = FileStream(file_abspath, encoding='utf8')
     lexer = ThriftLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = ThriftParser(stream)
     return parser
 
 
+def test_files():
+    # TODO: use *.thrift
+    files = glob.glob('./fixtures/tutorial.thrift')
+    for file in files:
+        parser = parse_one_thrift('../' + file)
+        ctx = ParserRuleContext()
+        parser.enterRule(ctx, 0, 0)
+        document = parser.document()
+        assert len(document.children) > 0
+
+
 def test_load_normal():
-    parser = parse_one_thrift('./tutorial.thrift')
+    parser = parse_one_thrift('../fixtures/tutorial.thrift')
     ctx = ParserRuleContext()
     parser.enterRule(ctx, 0, 0)
     document = parser.document()
