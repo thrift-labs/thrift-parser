63151c7fb47c57a3fac07a01bd52ef640a7ffad7
alingse <alingse@foxmail.com>
2022-05-29 23:22:54 +0800

    add ThriftData

--- thrift_parser/parse.py
+++ thrift_parser/parse.py
@@ -1,12 +1,18 @@
+from __future__ import annotations
+from typing import List, Optional
+
+from antlr4 import InputStream
 from antlr4 import FileStream
+from antlr4 import StdinStream
 from antlr4 import CommonTokenStream
 from antlr4 import ParserRuleContext
+from antlr4.Token import CommonToken
 
 from .ThriftLexer import ThriftLexer
 from .ThriftParser import ThriftParser
 
 
-def parse(input_stream, ctx=None):
+def parse(input_stream: InputStream, ctx: Optional[ParserRuleContext] = None):
     lexer = ThriftLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = ThriftParser(stream)
@@ -23,3 +29,21 @@ def parse(input_stream, ctx=None):
 def parse_file(file, ctx=None):
     input_stream = FileStream(file, encoding='utf8')
     return parse(input_stream, ctx=ctx)
+
+
+class ThriftData(object):
+
+    def __init__(self, input_stream: InputStream):
+        _, tokens, _, document = parse(input_stream)
+        self._tokens: List[CommonToken] = tokens.tokens
+        self.document: ThriftParser.DocumentContext = document
+
+    @classmethod
+    def from_file(cls, file: str) -> ThriftData:
+        input_stream = FileStream(file, encoding='utf8')
+        return cls(input_stream)
+
+    @classmethod
+    def from_stdin(cls) -> ThriftData:
+        input_stream = StdinStream(encoding='utf8')
+        return cls(input_stream)
