063bb71a82b016e578e9d45b2b0a104af4ac60c3
alingse <alingse@foxmail.com>
2022-06-08 21:01:38 +0800

    parse complex

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,5 +1,41 @@
+import antlr4 from 'antlr4';
 import ThriftParser from './ThriftParser.js';
+import ThriftLexer from './ThriftLexer.js';
 
 export {
     ThriftParser,
+    ThriftLexer,
+}
+
+export function parse(input_stream) {
+    const lexer = new ThriftLexer(input_stream);
+    const stream = new antlr4.CommonTokenStream(lexer);
+    const parser = new ThriftParser(stream);
+    const ctx = new antlr4.ParserRuleContext();
+    parser.enterRule(ctx, 0, 0);
+    const document = parser.document();
+    return [lexer, stream, parser, document];
+}
+
+export function parse_file(file) {
+    const input_stream = new antlr4.FileStream(file);
+    return parse(input_stream);
+}
+
+export class ThriftData {
+    constructor(input_stream) {
+        const [, tokens, , document] = parse(input_stream);
+        this.tokens = tokens;
+        this.document = document;
+    }
+
+    static from_file(file) {
+        const input_stream = new antlr4.FileStream(file);
+        return new ThriftData(input_stream);
+    }
+
+    static from_str(data) {
+        const input_stream = new antlr4.InputStream(data);
+        return new ThriftData(input_stream);
+    }
 }
