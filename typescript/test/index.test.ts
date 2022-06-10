54d695a4d55aafc79bd15876a851ea4fae061e95
alingse <alingse@foxmail.com>
2022-06-10 23:17:54 +0800

    add more test

--- typescript/test/index.test.ts
+++ typescript/test/index.test.ts
@@ -1,13 +1,21 @@
 import 'mocha';
 import assert  from 'assert';
 
+import { TerminalNode } from 'antlr4ts/tree';
 import { ThriftData } from '../src';
 
-describe('Array', function () {
-    describe('#indexOf()', function () {
-      it('should return -1 when the value is not present', function () {
+describe('Thrift Data', function () {
+    describe('test', function () {
+      it('load thrift from string', function () {
         const data = ThriftData.from_string('include "shared.thrift"');
         assert.strictEqual(data.tokens.get(0).text, 'include');
+        const header = data.document.getChild(0);
+        const include = header.getChild(0);
+        const token = include.getChild(0);
+        assert.strictEqual(token.text, 'include');
+        console.log(token);
+        assert.ok(token instanceof TerminalNode);
+        assert.strictEqual(token.childCount, 0);
       });
     });
 });
