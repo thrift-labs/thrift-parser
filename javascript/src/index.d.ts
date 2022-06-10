72e5d83029a55651da4a951c7fbedba27f7250e2
alingse <alingse@foxmail.com>
2022-06-10 20:08:49 +0800

    add lib

---
+++ javascript/src/index.d.ts
@@ -0,0 +1,15 @@
+export function parse(input_stream: antlr4.InputStream): any[];
+export function parse_file(file: string): any[];
+export class ThriftData {
+    static from_file(file: string): ThriftData;
+    static from_str(data: string): ThriftData;
+    constructor(input_stream: antlr4.InputStream);
+    tokens: antlr4.Token[];
+    document: ThriftParser.DocumentContext;
+}
+
+import antlr4 from 'antlr4';
+import ThriftParser from "./ThriftParser.js";
+import ThriftLexer from "./ThriftLexer.js";
+export { ThriftParser, ThriftLexer };
+//# sourceMappingURL=index.d.ts.map
