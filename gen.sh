9322ca14df6451178324c263a85a67740f6b8e14
alingse <alingse@foxmail.com>
2022-07-29 23:38:23 +0800

    update the true js

--- gen.sh
+++ gen.sh
@@ -3,6 +3,6 @@
 alias antlr4='java -jar /usr/local/lib/antlr-4.10.1-complete.jar'
 
 antlr4 -Dlanguage=Python3 Thrift.g4 -o python/thrift_parser
-antlr4 -Dlanguage=JavaScript Thrift.g4 -o ./javascript
+antlr4 -Dlanguage=JavaScript Thrift.g4 -o ./javascript/src
 # use antlr4ts
 typescript/node_modules/.bin/antlr4ts Thrift.g4 -o typescript/src
