2c3d4e91538617201c652c6ab3aa686f29aff748
alingse <alingse@foxmail.com>
2022-11-30 21:38:08 +0800

    upgrade to 1.0.6

--- gen.sh
+++ gen.sh
@@ -6,6 +6,8 @@ rm -rf python/thrift_parser/Thrift*
 antlr4 -Dlanguage=Python3 Thrift.g4 -o python/thrift_parser
 rm -rf javascript/src/Thrift*
 antlr4 -Dlanguage=JavaScript Thrift.g4 -o ./javascript/src
+
 # use antlr4ts
 cd typescript && npm install && cd ..
+rm -rf typescript/src/Thrift*
 typescript/node_modules/.bin/antlr4ts Thrift.g4 -o typescript/src
