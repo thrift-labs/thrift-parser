97ba721cc1c1c868524673ca9ae3bd6599008546
alingse <alingse@foxmail.com>
2024-11-21 10:51:50 +0000

    add test

--- java/src/test/java/thriftlabs/thriftparser/ParserTest.java
+++ java/src/test/java/thriftlabs/thriftparser/ParserTest.java
@@ -1,12 +1,33 @@
 package thriftlabs.thriftparser;
  
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
  
+import thriftlabs.thriftparser.ThriftLexer;
+import thriftlabs.thriftparser.ThriftParser;
+
 public class ParserTest {
  
     @Test
-    public void testMyMethod() {
-        assertEquals(10, 10, "The method did not return the expected result");
+    public void testParse() {
+        String content = "include \"./other.thrift\" \n" +
+"namespace py demo_thrift\n" +
+"namespace go demo_thrift\n" +
+"namespace java thriftlabs.thriftxx.demo_thrift\n" +
+"\n" +
+"const string __VERSION__ = \"0.0.8\"\n" +
+"\n" +
+"struct DemoStruct {" +
+    "1: required string x1;" +
+"}";
+        CharStream charStream = CharStreams.fromString(content);
+        ThriftLexer lexer = new ThriftLexer(charStream);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        ThriftParser parser = new ThriftParser(tokens);
+        
+        HeaderContext header = parser.header(0);
+        assertEquals("", header.String(), header.String());
     }
 }
