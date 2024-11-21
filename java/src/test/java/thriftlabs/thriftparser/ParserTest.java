28ff35ed9ca2f124c5f1e7431a2942273b680952
alingse <alingse@foxmail.com>
2024-11-21 10:39:53 +0000

    add code

---
+++ java/src/test/java/thriftlabs/thriftparser/ParserTest.java
@@ -0,0 +1,12 @@
+package thriftlabs.thriftparser;
+ 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+ 
+public class ParserTest {
+ 
+    @Test
+    public void testMyMethod() {
+        assertEquals(10, 10, "The method did not return the expected result");
+    }
+}
